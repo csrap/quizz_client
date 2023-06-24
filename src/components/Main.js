c819e6c55d3cc46af4460b6c9175f8b02927dc29
César Pinto <caugusto3110@gmail.com>
2023-06-24 09:28:25 +0200

    uptate140-v1

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,8 +6,16 @@ export default function Main() {
       <h1 className='title text-light'> Quiz Application</h1>
 
       <ul>
-        <li></li>
+        <li> You will be asked 10 questions one after another</li>
+        <li>10 points is awarded for the correct answer.</li>
+        <li>Each question has three options. You can choose only one options.</li>
+        <li>You can review and change answers beffore the quiz finish</li>
+        <li>The result will be declared at the end of the quiz</li>
       </ul>
+
+      <form id="form">
+        <input type="text" placeholder="Username" />
+      </form>
     </div>
   )
 }
