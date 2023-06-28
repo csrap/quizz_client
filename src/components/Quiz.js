dad3d63f132b5b4ff15492b78f9dded392d340cb
César Pinto <caugusto3110@gmail.com>
2023-06-28 21:50:37 +0200

    update145-v1

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Quiz() {
   return (
-    <div>Quiz</div>
+    <div className='container'>
+      <h1 className='title text-light'>
+        Quiz Application
+      </h1>
+    </div>
   )
 }
