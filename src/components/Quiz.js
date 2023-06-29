1ce8118a3aac91195592d3b607767263f46024ef
César Pinto <caugusto3110@gmail.com>
2023-06-29 19:40:37 +0200

    update144-v1

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -6,6 +6,11 @@ export default function Quiz() {
       <h1 className='title text-light'>
         Quiz Application
       </h1>
+
+      <div className='grid'>
+        <button>Prev</button>
+        <button>Max</button>
+      </div>
     </div>
   )
 }
