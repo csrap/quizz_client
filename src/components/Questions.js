7ee8983566cd5ac7506498a413b0878f791256c9
César Pinto <caugusto3110@gmail.com>
2023-07-04 17:20:13 +0200

    update145-v1

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -4,6 +4,15 @@ export const Questions = () => {
   return (
     <div className="questions">
       <h2 className='text-light'> Simple Question 1</h2>
+
+      <ul>
+        <li>
+          <input
+            type="radio"
+            value={true}
+          />
+        </li>
+      </ul>
     </div>
   )
 }
