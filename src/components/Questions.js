bc1915d446f3d84c460119957cf9a84f58958b4a
César Pinto <caugusto3110@gmail.com>
2023-07-07 22:36:19 +0200

    update152-v1

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Questions() {
+
+  const [checked, setchecked] = useState(undefined);
 
   function onSelect() {
     console.log('radio button change');
