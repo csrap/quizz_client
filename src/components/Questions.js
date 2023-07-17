5de8324cde7b6da09b0cd45b56159cad27ff9cf6
César Pinto <caugusto3110@gmail.com>
2023-07-17 18:11:42 +0200

    update164-v1

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,38 +1,47 @@
 import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import data from '../database/data'
 
 export default function Questions() {
 
-  const [checked, setchecked] = useState(undefined);
+  const [checked, setChecked] = useState(undefined)
+  const { trace } = useSelector(state => state.questions);
+  const result = useSelector(state => state.result.result);
+  const [{ isLoading, apiData, serverError }] = useFetchQestion()
 
-  const question = data[0]
+  const questions = useSelector(state => state.questions.queue[state.questions.trace])
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    console.log(question)
-  });
+    dispatch(updateResult({ trace, checked }))
+  }, [checked])
 
-  function onSelect() {
-    setchecked(true)
-    console.log('radio button change');
+  function onSelect(i) {
+    onChecked(i)
+    setChecked(i)
+    dispatch(updateResult({ trace, checked }))
   }
 
   return (
     <div className="questions">
       <h2 className='text-light'> Simple Question 1</h2>
 
-      <ul key={question}>
-        <li>
-          <input
-            type="radio"
-            value={false}
-            name="options"
-            id='q1-option'
-            onChange={onSelect}
-          />
+      <ul key={questions?.id}>
+        {
+          questions?.options.map((q, i) => (
+            <li key={i}>
+              <input
+                type="radio"
+                value={false}
+                name="options"
+                id={`q${i}-option`}
+                onChange={() => onSelect(i)}
+              />
 
-          <label className='text-primary' htmlFor="q1-option"> Option</label>
-          <div className='check checked'></div>
-        </li>
+
+            </li>
+          ))
+        }
       </ul>
     </div>
   )
