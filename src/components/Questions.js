454df528dde17a0fe6cfa46022c31aeca0fafb89
César Pinto <caugusto3110@gmail.com>
2023-07-18 22:04:22 +0200

    update-169-v1

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -38,6 +38,7 @@ export default function Questions() {
                 onChange={() => onSelect(i)}
               />
 
+              <label htmlFor={`q${i}-option`}></label>
 
             </li>
           ))
